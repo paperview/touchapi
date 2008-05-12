df05c4eb30ac4bdf9032f5c676f7ba68dedb3c5f
risenparadigm <risenparadigm@5e10ba55-a837-0410-a92f-dfacd3436b42>
2008-05-12 06:54:29 +0000

    Added mappings to UI objects for the following testApp parameters: threshold, lowRange, highRange, blurValue, blurGaussianValue, bInvertVideo, bVerticalMirror, and bHorizontalMirror

--- trunk/touch_tracker/tbeta/app/nuigroup/ofxCore_VS2005/src/uiDefinition.h
+++ trunk/touch_tracker/tbeta/app/nuigroup/ofxCore_VS2005/src/uiDefinition.h
@@ -1,53 +1,124 @@
 #ifndef __UI_DEFINITION
 #define __UI_DEFINITION
 
+#include "testApp.h"
 
-void setValue( float newVal, int arg1, int arg2, int arg3  )
+testApp *appPtr;
+
+void setThreshold(int newVal, int arg1, int arg2, int arg3)
 {
-	char idStr[1024];		
-	sprintf(idStr, "Value: %f\nArg1: %i\nArg2: %i\nArg3: %i\n",newVal,arg1,arg2,arg3);
-	printf(idStr);
+	appPtr->threshold = newVal;
 }
-
-
+void setLowLevel(int newVal, int arg1, int arg2, int arg3)
+{
+	appPtr->lowRange = newVal;
+}
+void setHighLevel(int newVal, int arg1, int arg2, int arg3)
+{
+	appPtr->highRange = newVal;
+}
+void setBlur(int newVal, int arg1, int arg2, int arg3)
+{
+	//smoothing kernel must be odd
+	appPtr->blurValue = newVal+((newVal%2)?0:1);
+}
+void setGaussianBlur(int newVal, int arg1, int arg2, int arg3)
+{
+	//smoothing kernel must be odd
+	appPtr->blurGaussianValue = newVal+((newVal%2)?0:1);
+}
+void setInvert(int arg1, int arg2, int arg3)
+{
+	appPtr->bInvertVideo = !appPtr->bInvertVideo;
+}
+void setMirrorVertical(int arg1, int arg2, int arg3)
+{
+	appPtr->bVerticalMirror = !appPtr->bVerticalMirror;
+}
+void setMirrorHorizontal(int arg1, int arg2, int arg3)
+{
+	appPtr->bHorizontalMirror = !appPtr->bHorizontalMirror;
+}
 
 void testApp::setupUI()
 {
-
+	appPtr = this;
 	AParameterUI* ui = AParameterUI::Instance();
-	AParameterUIPanel* tmpPanel_1 = ui->addPanel( "Video Source", 685.0f, 35.0f );
-	AParameterUIPanel* tmpPanel_2 = ui->addPanel( "Video Settings", 685.0f, 235.0f );
-	AParameterUIPanel* tmpPanel_3 = ui->addPanel( "Effects & Filters", 685.0f, 445.0f );
+	AParameterUIPanel* tmpPanel_1 = 
+		ui->addPanel("Video Source", 685.0f, 35.0f);
+	AParameterUIPanel* tmpPanel_2 =
+		ui->addPanel("Video Settings", 685.0f, 235.0f);
+	AParameterUIPanel* tmpPanel_3 =
+		ui->addPanel("Effects & Filters", 685.0f, 445.0f);
 
-	AParameterUIObjectPushButton*	tmpOnePushButton2 = tmpPanel_1->addPushButton( "Active" );
-	AParameterUIObjectSliderInteger* tmpIntegerSlider = tmpPanel_1->addSliderInteger( "ID", 300, 1, 100, 100 );
-	AParameterUIObjectPushButton*	tmpOnePushButton1b = tmpPanel_1->addPushButton( "Camera" );
-	AParameterUIObjectPushButton*	tmpOnePushButton2b = tmpPanel_1->addPushButton( "VCR" );
+	AParameterUIObjectPushButton* tmpOnePushButton1 = 
+		tmpPanel_1->addPushButton("Active");
+	AParameterUIObjectPushButton* tmpOnePushButton2 = 
+		tmpPanel_1->addPushButton("Camera");
+	AParameterUIObjectPushButton* tmpOnePushButton3 = 
+		tmpPanel_1->addPushButton("Video");
+	AParameterUIObjectSliderInteger* tmpIntegerSlider1 = 
+		tmpPanel_1->addSliderInteger("ID", 300, 1, 100, 100);
 
-	AParameterUIObjectSliderFloat* tmpFloatSlider3 = tmpPanel_2->addSliderFloat( "Threshold", 300, 0.0f, 360, 0.0f );
-	AParameterUIObjectSliderFloat* tmpFloatSlider2a = tmpPanel_2->addSliderFloat( "Low Level", 300, 1.0f, 300, 1 );	
-	AParameterUIObjectSliderFloat* tmpFloatSlider2 = tmpPanel_2->addSliderFloat( "High Level", 300, 1.0f, 300, 1 );
-	tmpFloatSlider2->changedParameterCallback = &setValue;
-	tmpFloatSlider2->funcPointerSet = true;
+	AParameterUIObjectSliderInteger* tmpIntegerSlider2 = 
+		tmpPanel_2->addSliderInteger("Threshold", 300, 0, 255, 99);
+	tmpIntegerSlider2->changedParameterCallback = &setThreshold;
+	tmpIntegerSlider2->funcPointerSet = true;
 
-	AParameterUIObjectSliderFloat* tmpSpinSlider = tmpPanel_3->addSliderFloat( "Blur", 300, 0.0f, 360.0f, 0.0f );
-	tmpSpinSlider->changedParameterCallback = &setValue;
-	tmpSpinSlider->funcPointerSet = true;
+	AParameterUIObjectSliderInteger* tmpIntegerSlider3 = 
+		tmpPanel_2->addSliderInteger("Low Level", 300, 0, 255, 0);	
+	tmpIntegerSlider3->changedParameterCallback = &setLowLevel;
+	tmpIntegerSlider3->funcPointerSet = true;
 
-	AParameterUIObjectSliderFloat* tmpFloatSlider = tmpPanel_3->addSliderFloat( "Gasian Blur", 300, 0.0f, 1.0f, 0.0f );
-	AParameterUIObjectSliderInteger* tmpIntegerSliderx = tmpPanel_3->addSliderInteger( "Background Subtract", 300, 1, 100, 100 );
-	AParameterUIObjectPushButton*	tmpOnePushButton1x = tmpPanel_3->addPushButton( "Clear" );
-	AParameterUIObjectPushButton*	tmpOnePushButton2x = tmpPanel_3->addPushButton( "Invert" );
-	AParameterUIObjectPushButton*	tmpOnePushButton3x = tmpPanel_3->addPushButton( "Warp" );
-	AParameterUIObjectPushButton*	tmpOnePushButton4x = tmpPanel_3->addPushButton( "Mirror Vertical" );
-	AParameterUIObjectPushButton*	tmpOnePushButton5x = tmpPanel_3->addPushButton( "Mirror Horizontal" );
+	AParameterUIObjectSliderInteger* tmpIntegerSlider4 = 
+		tmpPanel_2->addSliderInteger("High Level", 300, 0, 255, 255);
+	tmpIntegerSlider4->changedParameterCallback = &setHighLevel;
+	tmpIntegerSlider4->funcPointerSet = true;
 
+	AParameterUIObjectSliderInteger* tmpIntegerSlider5 = 
+		tmpPanel_3->addSliderInteger("Blur", 300, 0, 
+									 min(camWidth, camHeight), 1);
+	tmpIntegerSlider5->changedParameterCallback = &setBlur;
+	tmpIntegerSlider5->funcPointerSet = true;
+
+	AParameterUIObjectSliderInteger* tmpIntegerSlider6 = 
+		tmpPanel_3->addSliderInteger("Gaussian Blur", 300, 0,
+								     min(camWidth, camHeight), 1);
+	tmpIntegerSlider6->changedParameterCallback = &setGaussianBlur;
+	tmpIntegerSlider6->funcPointerSet = true;
+
+
+	AParameterUIObjectSliderInteger* tmpIntegerSlider7 = 
+		tmpPanel_3->addSliderInteger("Background Subtract", 300, 1, 100, 100);
+
+	AParameterUIObjectPushButton* tmpOnePushButton4 = 
+		tmpPanel_3->addPushButton("Clear");
+
+	AParameterUIObjectPushButton* tmpOnePushButton5 = 
+		tmpPanel_3->addPushButton("Invert");
+	tmpOnePushButton5->changedParameterCallback = &setInvert;
+	tmpOnePushButton5->funcPointerSet = true;
+
+	AParameterUIObjectPushButton* tmpOnePushButton6 = 
+		tmpPanel_3->addPushButton("Warp");
+
+	AParameterUIObjectPushButton* tmpOnePushButton7 = 
+		tmpPanel_3->addPushButton("Mirror Vertical");
+	tmpOnePushButton7->changedParameterCallback = &setMirrorVertical;
+	tmpOnePushButton7->funcPointerSet = true;
+
+	AParameterUIObjectPushButton* tmpOnePushButton8 = 
+		tmpPanel_3->addPushButton("Mirror Horizontal");
+	tmpOnePushButton7->changedParameterCallback = &setMirrorHorizontal;
+	tmpOnePushButton7->funcPointerSet = true;
 /*
-	AParameterUIObjectCheckbox*	tmpCheckBox = tmpPanel_1->addCheckbox( "Lighting", exit );
+	AParameterUIObjectCheckbox*	tmpCheckBox = 
+	tmpPanel_1->addCheckbox( "Lighting", exit );
 	tmpCheckBox->changedParameterCallback = &testApp::exit;
 	tmpCheckBox->funcPointerSet = true;
 
-	AParameterUIObjectPushButton*	tmpPushButton = tmpPanel_1->addPushButton( "PushButton" );
+	AParameterUIObjectPushButton*	tmpPushButton = 
+	tmpPanel_1->addPushButton( "PushButton" );
 	tmpPushButton->changedParameterCallback = &buttonPressed;
 	tmpPushButton->funcPointerSet = true;
 */
